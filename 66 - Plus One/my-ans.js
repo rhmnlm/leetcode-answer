4cbfd9463e20f4d5175b373f1bd21d3bd6f2a203
rhmnlm <rahmannilam@gmail.com>
2026-01-01 15:47:24 +0800

    learn:daily leetcode

--- 66 - Plus One/my-ans.js
+++ 66 - Plus One/my-ans.js
@@ -1,22 +1,44 @@
+// /**
+//  * @param {number[]} digits
+//  * @return {number[]}
+//  */
+// var plusOne = function(digits) {
+//     let carry = 1;
+//     for(let i = digits.length-1; i >= 0; i--){
+//         if(digits[i] == 9){
+//             digits[i] = 0;
+//             carry = 1;
+//             continue;
+//         } else if(carry == 1){
+//             digits[i]++;
+//             carry = 0;
+//             break;
+//         }
+//     }
+
+//     if(carry == 1) digits.unshift(1);
+
+//     return digits;
+// };
+
 /**
  * @param {number[]} digits
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let carry = 1;
-    for(let i = digits.length-1; i >= 0; i--){
-        if(digits[i] == 9){
-            digits[i] = 0;
+    let n = digits.length, carry = 0;
+
+    for(let i=n-1; i>= 0; i--){
+        if(digits[i]==9){
             carry = 1;
-            continue;
-        } else if(carry == 1){
-            digits[i]++;
+            digits[i] = 0
+        } else {
             carry = 0;
+            digits[i]++;
             break;
         }
+        if(carry == 1 && i==0) digits.unshift(1);
     }
-
-    if(carry == 1) digits.unshift(1);
 
     return digits;
 };
