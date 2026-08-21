c37a48714f3a49edf83e4f45931b0091e6a41d2e
rhmnlm <rahmannilam@gmail.com>
2026-08-21 21:53:37 +0800

    learn: daily leetcode

---
+++ 349 - Intersection of Two Arrays/my-ans.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+
+    return [...set1.intersection(set2)];
+};
